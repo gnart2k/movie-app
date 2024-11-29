1cc2ea5c132dfb80896dbc5c9235195e659a50e3
ittuan <tuan2k1@gmail.com>
2024-11-29 13:37:24 +0700

    add icon button widget

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/core/widgets/button/common_button.dart';
+import 'package:movie_app/core/widgets/button/icon_button.dart';
 import 'package:movie_app/core/widgets/footer_widget.dart';
 import 'package:movie_app/core/widgets/header_widgets.dart';
 
@@ -13,6 +14,8 @@ class HomeScreen extends StatelessWidget {
       children: [
         HeaderWidgets(),
         CommonButton(label: "fsa", onTap: () {}),
+        CustomIconButton(
+            label: 'cvasfdsa', onTap: () {}, iconData: Icons.play_arrow),
         FooterWidget(),
       ],
     ));
