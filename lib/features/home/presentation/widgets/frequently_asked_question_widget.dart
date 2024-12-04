dbbb85c88a7a6ca1113f9e54196fb35b84d7191d
Linhng5 <nguyenlinh0912202@gmail.com>
2024-12-05 03:39:34 +0700

    feature/add_change_locale: add change locale vi en and ko

--- lib/features/home/presentation/widgets/frequently_asked_question_widget.dart
+++ lib/features/home/presentation/widgets/frequently_asked_question_widget.dart
@@ -4,6 +4,7 @@ import 'package:movie_app/core/widgets/button/common_button.dart';
 import 'package:movie_app/core/widgets/title/common_title.dart';
 import 'package:movie_app/features/home/data/models/faq_section.dart';
 import 'package:movie_app/features/home/presentation/view_models/faq_section_viewmodel.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FrequentlyAskedQuestionsWidget extends ConsumerStatefulWidget {
   const FrequentlyAskedQuestionsWidget({super.key});
@@ -56,7 +57,7 @@ class FrequentlyAskedQuestionsWidgetState
             },
           ),
         ),
-        SizedBox(width: 40),
+        const SizedBox(width: 40),
         Expanded(
           child: ListView.builder(
             shrinkWrap: true,
@@ -85,7 +86,7 @@ class FrequentlyAskedQuestionsWidgetState
                   "Got questions? We've got answers! Check out our FAQ section to find answers to the most common questions about StreamVibe.",
             ),
           ),
-          CommonButton(label: "Ask a question", onTap: () {}),
+          CommonButton(label: AppLocalizations.of(context)!.ask_a_question, onTap: () {}),
         ],
       ),
     );
