3e934d210ea3e752ed4153cfed23c0d3b68bf0e7
Linhng5 <nguyenlinh0912202@gmail.com>
2024-12-04 12:49:24 +0700

    feature/show_home_page: add layout

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         // home: MovieOpenPage());
         // home: MovieAndShowPage());
-        home: const HomeScreen());
+        home: const MovieAndShowPage());
     // home: const SupportPage());
   }
 }
