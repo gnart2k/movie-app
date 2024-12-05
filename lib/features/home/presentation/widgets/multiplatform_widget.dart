1dca7a636d07c691c24cae77df40068bfb94ecc6
Linhnguyen202 <ngglinh0914@gmail.com>
2024-12-05 22:08:43 +0700

    fixbug/fixui: fix mutilplatform overflow text

--- lib/features/home/presentation/widgets/multiplatform_widget.dart
+++ lib/features/home/presentation/widgets/multiplatform_widget.dart
@@ -34,112 +34,112 @@ class MultiPlatformWidget extends ConsumerWidget {
       devices.length,
       (index) => {'icon': deviceIcons[index], 'title': devices[index]},
     );
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 150),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            homeProps.deviceCompatibilitiy.title,
-            style: GoogleFonts.manrope(
-              fontSize: 38,
-              fontWeight: FontWeight.w700,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          homeProps.deviceCompatibilitiy.title,
+          style: GoogleFonts.manrope(
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
           ),
-          const SizedBox(height: 14),
-          Text(
-            AppLocalizations.of(context)!.description_streamvibe,
-            style: GoogleFonts.manrope(
-              fontSize: 18,
-              fontWeight: FontWeight.w400,
-              color: AppColors.lightGray,
-            ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          AppLocalizations.of(context)!.description_streamvibe,
+          style: GoogleFonts.manrope(
+            fontSize: 18,
+            fontWeight: FontWeight.w400,
+            color: AppColors.lightGray,
           ),
-          const SizedBox(height: 80),
-          GridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 30,
-              mainAxisSpacing: 30,
-              childAspectRatio: 1.75,
-            ),
-            itemCount: platforms.length,
-            itemBuilder: (context, index) {
-              final platform = platforms[index];
-              return Container(
-                decoration: const BoxDecoration(),
-                child: Card(
-                  color: AppColors.darkGray,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: const LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          AppColors.linearGardientColor,
-                          Colors.transparent,
-                        ],
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(color: AppColors.cardBorder, width: 1),
+        ),
+        const SizedBox(height: 20),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 30,
+            mainAxisSpacing: 30,
+            childAspectRatio: 1.75,
+          ),
+          itemCount: platforms.length,
+          itemBuilder: (context, index) {
+            final platform = platforms[index];
+            return Container(
+              decoration: const BoxDecoration(),
+              child: Card(
+                color: AppColors.darkGray,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        AppColors.linearGardientColor,
+                        Colors.transparent,
+                      ],
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(50),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: AppColors.appBackground,
-                                  borderRadius: BorderRadius.circular(12),
-                                  border: Border.all(
-                                    color: AppColors.topBarBorder,
-                                    width: 1,
-                                  ),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16),
-                                  child: SvgPicture.asset(
-                                    platform['icon']!,
-                                  ),
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: AppColors.cardBorder, width: 1),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: AppColors.appBackground,
+                                borderRadius: BorderRadius.circular(12),
+                                border: Border.all(
+                                  color: AppColors.topBarBorder,
+                                  width: 1,
                                 ),
                               ),
-                              const SizedBox(width: 16),
-                              Text(
-                                platform['title']!,
-                                style: GoogleFonts.manrope(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w600,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16),
+                                child: SvgPicture.asset(
+                                  platform['icon']!,
+                                  width: 20,
                                 ),
                               ),
-                            ],
-                          ),
-                          const SizedBox(height: 30),
-                          Text(
-                            AppLocalizations.of(context)!.app_description,
-                            style: GoogleFonts.manrope(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400,
-                              color: AppColors.lightGray,
                             ),
+                            const SizedBox(width: 16),
+                            Text(
+                              platform['title']!,
+                              style: GoogleFonts.manrope(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          AppLocalizations.of(context)!.app_description,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.manrope(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: AppColors.lightGray,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              );
-            },
-          ),
-        ],
-      ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
